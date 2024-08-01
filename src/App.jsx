f5c696c2fd0f1ee7b1095c4b480fefb5d5c7b963
Mr-H4ck3r-18 <rksasank2000@gmail.com>
2024-08-01 22:08:37 +0530

    Final Project Management APP

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,38 @@ import NewProject from "./components/NewProject";
 import SideBar from "./components/SideBar";
 import { useState } from "react";
 import Dashboard from "./components/Dashboard";
+import SelectedProject from "./components/SelectedProject";
+
 
 function App() {
   const [projectState, setProjectState] = useState({
     selectedProjectID: undefined,
-    projects: []
+    projects: [],
+    tasks:[]
   })
+  function handleAddTask(text){
+    setProjectState(prev=>{
+      const randomID=Math.random()
+      const newTask ={
+        text:text,
+        projectID:prev.selectedProjectID,
+        id:randomID
+      }
+      return {
+        ...prev,
+        tasks:[...prev.tasks,newTask]
+      }
+    })
+
+  }
+  function handleDeleteTask(id){
+    setProjectState((prev => {
+      return {
+        ...prev,
+        tasks:prev.tasks.filter((task)=>task.id!==id)
+      }
+    }))
+  }
   const handleNewProject = () => {
     setProjectState((prev => {
       return {
@@ -40,19 +66,40 @@ function App() {
     })
   }
 
-  let content;
+  const selectedProject=projectState.projects.find(project=>project.id===projectState.selectedProjectID)
+
+  let content = <SelectedProject project={selectedProject} onDelete={handleDelete} onAdd={handleAddTask} onDeleteTask={handleDeleteTask} tasks={projectState.tasks}/>;
   if(projectState.selectedProjectID===null){
     content=<NewProject onAdd={handleData} click={handleCancel} />
   }else if (projectState.selectedProjectID===undefined){
     content=<Dashboard click={handleNewProject} />
   }
 
-  
+  function handleSelectProject(id){
+    setProjectState((prev => {
+      return {
+        ...prev,
+        selectedProjectID: id
+      }
+    }))
+  }
 
-  console.log(projectState)
+  function handleDelete(){
+    setProjectState((prev => {
+      return {
+        ...prev,
+        selectedProjectID: undefined,
+        projects:prev.projects.filter((project)=>project.id!==projectState.selectedProjectID )
+      }
+    }))
+  }
+
+
+
+
   return (
     <main className="my-8 h-screen flex gap-8">
-      <SideBar click={handleNewProject} projects={projectState.projects} />
+      <SideBar click={handleNewProject} projects={projectState.projects} onSelectProject={handleSelectProject} selectedProjectID={projectState.selectedProjectID}/>
       {content}
     </main>
   );
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import Button from "./Button"
-export default function SideBar({ click, projects }) {
+export default function SideBar({ click, projects, onSelectProject, selectedProjectID }) {
     return (
         <aside className=" bg-stone-900 w-1/3 rounded-r-xl px-8 py-16 md:w-72 text-stone-50">
             <h2 className="text-stone-200 mb-8 font-bold uppercase md:text-xl">Your Project</h2>
@@ -9,9 +9,19 @@ export default function SideBar({ click, projects }) {
                 > + Add Project</Button>
             </div>
             <ul className="mt-8">
-                {projects.map(project => (<li key={project.id}>
-                    <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">{project.title}</button>
-                </li>))}
+                {projects.map(project => {
+                    let cssClasses = "w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800 "
+                    if (selectedProjectID === project.id) {
+                        cssClasses += "bg-stone-800 text-stone-200"
+                    } else {
+                        cssClasses += "text-stone-400"
+                    }
+                    return (
+                        <li key={project.id}>
+                            <button onClick={() => { onSelectProject(project.id) }} className={cssClasses}>{project.title}</button>
+                        </li>
+                    )
+                })}
             </ul>
         </aside>
     )
